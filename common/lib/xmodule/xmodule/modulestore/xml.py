567999bdc888e6d09a40f66926a257396ef02d00
Ayub khan <muhammadayubkhan6@gmail.com>
2019-08-19 18:47:51 +0500

    BOM-90 Made all tests py3 compatible.

--- common/lib/xmodule/xmodule/modulestore/xml.py
+++ common/lib/xmodule/xmodule/modulestore/xml.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
+import codecs
 import glob
 import hashlib
+import io
 import itertools
 import json
 import logging
@@ -454,7 +456,7 @@ class XMLModuleStore(ModuleStoreReadBase):
             # VS[compat]
             # TODO (cpennington): Remove this once all fall 2012 courses have
             # been imported into the cms from xml
-            course_file = BytesIO(clean_out_mako_templating(course_file.read()))
+            course_file = BytesIO(six.b(clean_out_mako_templating(course_file.read())))
 
             course_data = etree.parse(course_file, parser=edx_xml_parser).getroot()
 
@@ -644,7 +646,7 @@ class XMLModuleStore(ModuleStoreReadBase):
             if filepath.endswith('~'):  # skip *~ files
                 continue
 
-            with open(filepath) as f:
+            with io.open(filepath) as f:
                 try:
                     if filepath.find('.json') != -1:
                         # json file with json data content
@@ -666,7 +668,7 @@ class XMLModuleStore(ModuleStoreReadBase):
                         slug = os.path.splitext(os.path.basename(filepath))[0]
                         loc = course_descriptor.scope_ids.usage_id.replace(category=category, name=slug)
                         # html file with html data content
-                        html = f.read().decode('utf-8')
+                        html = f.read()
                         try:
                             module = system.load_item(loc)
                             module.data = html
